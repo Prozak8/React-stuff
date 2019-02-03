31bfdee571d81700ba81f345ce13bc462d2fb99f
Zak <zakary.howard94@gmail.com>
2019-02-03 15:44:10 +0100

    letter counter component created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import ValidationText from './ValidationText/ValidationText';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Letter Counter App</h1>
         <ul>
-          <li>Create an input field (in APP component) with a change lstener that outputs the length of the entered text below it (e.g. in a paragraph)</li>
+          <li>Create an input field (in APP component) with a change listener that outputs the length of the entered text below it (e.g. in a paragraph)</li>
           <li>Create a new component (=>ValidationText) that receives the text length as a prop.</li>
           <li>Inside the ValidationText component, either output "text too short" or "Text long enough" depending on the text length (e.g. take 5 as minumum length)</li>
           <li>Create another component (=> CharComponent) and style it as an inline box (=> display: inline-block, padding: 16px, text-align: center, margin: 16px, border: 1px solid black)</li>
@@ -15,6 +17,10 @@ class App extends Component {
           <li>When you click a CharComponent, it should be removed from the entered text.</li>
         </ul>
         <p>Hint: Keep in mind that JavaScript strings are basically arrays!</p>
+
+        <ValidationText />
+        <input type="text" className="input-field"></input>
+
       </div>
     );
   }
--- src/ValidationText/ValidationText.js
+++ src/ValidationText/ValidationText.js
@@ -1,1 +1,11 @@
 import React from 'react';
+
+const validationText = ( props ) => {
+    return (
+        <div className="ValidationText">
+            <p>123</p>
+        </div>
+    );
+};
+
+export default validationText;
